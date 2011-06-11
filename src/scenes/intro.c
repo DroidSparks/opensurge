82b9ccf6d03dbb2e94cc278881251f3eb9054838
alemart <alemart@0bdd0920-9fb6-4f02-91b4-be6900bfba83>
2011-06-11 02:39:55 +0000

    improved the editor grid. they now show 64x64 divisions

--- src/scenes/intro.c
+++ src/scenes/intro.c
@@ -60,7 +60,7 @@ static char *text =
  "along with this program; if not, write to the Free Software Foundation,\n"
  "Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.\n"
  "\n"
- "Please check the licenses folder for more information.\n"
+ "This game was made using the Open Surge Engine.\n"
 ;
 
 
