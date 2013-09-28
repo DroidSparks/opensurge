d2bd05c7af7ed1cc4bd2898433027f42d2374c69
alemart <alemart@0bdd0920-9fb6-4f02-91b4-be6900bfba83>
2013-09-28 16:42:31 +0000

    fixed osspec.c for freebsd

--- src/core/osspec.c
+++ src/core/osspec.c
@@ -615,7 +615,7 @@ void list_directory_recursively(xptable_t *table, const char *wildcard)
     strcat(ilovesurge, "*"); /* example: "/home/alexandre/.opensurge/sprites/ *" */
 
     /* list the files matching the wildcard */
-    if(al_findfirst(wildcard, &info, FA_ALL & ~FA_LABEL & ~FA_DIREC) == 0) {
+    if(al_findfirst(wildcard, &info, FA_ALL/* & ~FA_LABEL & ~FA_DIREC*/) == 0) {
         do {
             if(!(info.attrib & FA_DIREC) && !(info.attrib & FA_LABEL)) { /* why do I need to check this? can't trust Allegro on this one? */
                 char *file = mallocx((strlen(current_dir) + strlen(info.name) + 1) * sizeof(*file));
@@ -629,7 +629,7 @@ void list_directory_recursively(xptable_t *table, const char *wildcard)
     }
 
     /* look inside the directories, recursively */
-    if(al_findfirst(ilovesurge, &info, FA_DIREC) == 0) {
+    if(al_findfirst(ilovesurge, &info, FA_ALL/*FA_DIREC*/) == 0) { /* FA_DIREC doesn't work on BSD?? */
         do {
             if((info.attrib & FA_DIREC) && strcmp(info.name, "") != 0 && strcmp(info.name, ".") != 0 && strcmp(info.name, "..") != 0) {
                 char *new_wildcard = mallocx((strlen(current_dir) + strlen(info.name) + strlen(curinga) + 2) * sizeof(*new_wildcard));
